ffd2848e4aa6a2931704ce83cd80e13b7542a130
Students0256 <atcoderstudents0256@gmail.com>
2021-12-25 17:42:53 +0900

    Next.js ts ESLint Prettier

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
 }
+
+/**
+module.exports = {
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      loader: 'eslint-loader',
+      exclude: /node_modeles/
+    })
+    return config
+  }
+}
+*/
